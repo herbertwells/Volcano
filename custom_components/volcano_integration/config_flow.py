81267fbefe388a0ad509a8f2d58265b0b53c7c47
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2024-12-29 22:55:33 +0000

    Create config_flow.py

---
+++ custom_components/volcano_integration/config_flow.py
@@ -0,0 +1,28 @@
+"""Config flow for the Volcano Integration."""
+import logging
+
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.core import callback
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+class VolcanoIntegrationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Volcano Integration."""
+    
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        if user_input is not None:
+            # We don't actually need user-provided data, since everything is static.
+            return self.async_create_entry(title="Volcano Integration", data={})
+
+        # No fields – just a Submit button
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({}),  # empty form
+        )
