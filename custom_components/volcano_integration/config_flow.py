d4c0f4bb3c7a12bc16b02351d5f642210138fd9c
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2024-12-31 02:28:25 +0000

    Update config_flow.py

--- custom_components/volcano_integration/config_flow.py
+++ custom_components/volcano_integration/config_flow.py
@@ -9,14 +9,13 @@ from . import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-
 class VolcanoIntegrationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Volcano Integration."""
-
+    
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step with just a Submit button."""
+        """Handle the initial step for the user to 'Submit'."""
         if user_input is not None:
             return self.async_create_entry(title="Volcano Integration", data={})
 
