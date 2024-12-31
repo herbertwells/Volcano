46d269ec91166ef75006cc338a83351e20cd0809
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2024-12-31 01:29:20 +0000

    Update const.py

--- custom_components/volcano_integration/const.py
+++ custom_components/volcano_integration/const.py
@@ -1,2 +1,9 @@
 """Constants for the Volcano Integration."""
+
 DOMAIN = "volcano_integration"
+
+# Possible Bluetooth status strings
+BT_STATUS_DISCONNECTED = "DISCONNECTED"
+BT_STATUS_CONNECTING = "CONNECTING"
+BT_STATUS_CONNECTED = "CONNECTED"
+BT_STATUS_ERROR = "ERROR"
