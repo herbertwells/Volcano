bb7eb13d57dd2c33bbb519c364f37397980d2ba8
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2025-01-01 15:18:12 +0000

    Update config_flow.py

--- custom_components/volcano_integration/config_flow.py
+++ custom_components/volcano_integration/config_flow.py
@@ -1,6 +1,5 @@
 """Config flow for Volcano Integration."""
 import logging
-import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.core import callback
@@ -9,18 +8,38 @@ from . import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-
-class VolcanoIntegrationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class VolcanoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Volcano Integration."""
 
     VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step with a simple Submit form."""
+        """Handle the initial step."""
         if user_input is not None:
-            return self.async_create_entry(title="Volcano Integration", data={})
+            return self.async_create_entry(title="Volcano Vaporizer", data=user_input)
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({}),  # empty form
+            data_schema=vol.Schema({
+                vol.Required("name", default="Volcano Vaporizer"): str,
+                vol.Required("mac_address"): str,
+            }),
         )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Get the options flow for this handler."""
+        return VolcanoOptionsFlowHandler(config_entry)
+
+class VolcanoOptionsFlowHandler(config_entries.OptionsFlow):
+    """Handle Volcano Integration options."""
+
+    def __init__(self, config_entry):
+        """Initialize."""
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        """Manage the Volcano options."""
+        return self.async_create_entry(title="", data=user_input or {})
