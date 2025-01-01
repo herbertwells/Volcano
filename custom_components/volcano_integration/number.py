bc43a94c5823e48e7c6fa922d3f4595e9fa91995
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2025-01-01 04:26:44 +0000

    Update number.py

--- custom_components/volcano_integration/number.py
+++ custom_components/volcano_integration/number.py
@@ -30,7 +30,7 @@ class VolcanoHeaterTempNumber(NumberEntity):
     def __init__(self, manager):
         self._manager = manager
         self._attr_name = "Volcano Heater Temperature Setpoint"
-        self._attr_unique_id = "volcano_heater_temperature_setpoint"
+        self._attr_unique_id = "volcano_heater_setpoint"  # Reverted to original unique_id
         self._attr_entity_category = EntityCategory.CONFIG
 
         # Set the allowed range
